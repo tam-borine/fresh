31f824e33f584740e6474837fe02dba960025509
Tam <tam@Tams-MacBook.local>
2016-12-20 20:27:42 +0000

    containers/index.js hello world

---
+++ Hippocampus/ReactApp/containers/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* Setup ==================================================================== */
+import React, { Component } from 'react'
+import {
+	Navigator,
+	StatusBar,
+	Text,
+	View
+} from 'react-native'
+
+console.log("anyone here by now")
+
+import NavigationBar from 'react-native-navbar'
+import SideMenu from 'react-native-side-menu'
+
+export default class AppContainer extends Component {
+	render () {
+		return (
+			<Text>Hello world</Text>
+			
+			);
+	}
+}
