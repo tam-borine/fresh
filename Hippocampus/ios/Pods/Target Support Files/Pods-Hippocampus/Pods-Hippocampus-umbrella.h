e927332cca97483b7723ebab1881d674719a7d3d
Tam <tam@Tams-MacBook.home>
2016-12-23 21:07:44 +0000

    installed firestack and podfile custom wrote -override bug workaround from link- and linked firestack to react-native

--- Hippocampus/ios/Pods/Target Support Files/Pods-Hippocampus/Pods-Hippocampus-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_HippocampusVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HippocampusVersionString[];
-
