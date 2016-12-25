f307b9683c4e431edebba3754d8aa5b7f7b69e75
Tam <tam@Tams-MacBook.local>
2016-12-25 23:04:14 +0000

    header for signInPage done mvp

--- Hippocampus/ReactApp/components/signInPage/signInHeader.js
+++ Hippocampus/ReactApp/components/signInPage/signInHeader.js
@@ -4,7 +4,8 @@ import React, {Component} from 'react'
 import {
 	View,
 	Text,
-	Dimensions
+	Dimensions,
+	Image
 } from 'react-native'
 
 export default class SignInHeader extends Component {
@@ -12,8 +13,15 @@ export default class SignInHeader extends Component {
 		var {height, width} = Dimensions.get('window');
 		return (
 			<View style={{width: width, height: 150, backgroundColor: 'red'}}>
-				<Text style={{textAlign: 'center', color: '#ffffff'}}>Hippocampus</Text>
-				<Text>Making doctors' lives easier</Text>
+				<Text/>
+				<Text/>
+				<Image
+				    style={{width: 110, height: 75}}
+				    alignSelf='center'
+				    source={require('./images/logo_white.png')}
+				/>
+				<Text style={{textAlign: 'center', color: '#ffffff', fontSize: 20}}>Hippocampus</Text>
+				<Text style={{textAlign: 'center', color: '#ffffff'}}>Making doctors' lives easier</Text>
 			</View>
 
 			)
