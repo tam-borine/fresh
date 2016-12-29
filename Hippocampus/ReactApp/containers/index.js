db3f45f2e734b4e2a00a2b6c504e7dd2983d146c
Alfie <alfie_ab@hotmail.co.uk>
2016-12-29 11:17:42 +0000

    index added again

---
+++ Hippocampus/ReactApp/containers/index.js
@@ -0,0 +1,36 @@
+
+'use strict';
+
+
+/* Setup ==================================================================== */
+import React, { Component } from 'react'
+import {
+	Navigator,
+	StatusBar,
+	Text,
+	View,
+	Button
+} from 'react-native'
+
+// var auth = require('../oauth.js')
+// import Anonymous from '../anon'
+// import EmailLogin from '../email'
+// import Example from '../linkedIn.js'
+// import SignInPage from '../components/signInPage/index'
+import Feed from '../components/feeds/index'
+import Test from '../components/sidemenu/index'
+import NavigationBar from 'react-native-navbar'
+import SideMenu from 'react-native-side-menu'
+
+export default class AppContainer extends Component {
+	render () {
+		return (
+			<Test/>
+			);
+	}
+}
+
+//			<Anonymous title="Agnostic Trigger Button" onPress={() => fir.testingFirebase()}>Click me to test</Anonymous>
+
+
+// ref.writePost('My awesome post', 'some ranting about doctors lives')
