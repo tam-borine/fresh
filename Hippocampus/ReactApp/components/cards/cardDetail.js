fdb85cfbc41f5478f8ba8e9d53d9b44fd6abc31f
Alfie <alfie_ab@hotmail.co.uk>
2016-12-30 17:06:39 +0000

    further styling

--- Hippocampus/ReactApp/components/cards/cardDetail.js
+++ Hippocampus/ReactApp/components/cards/cardDetail.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './card'
 import CardSection from './cardSection'
 
+// Below is hard coded just for styling purposes, will need to make it flexible later
+// First view box is empty now but will be filled with image
 const CardDetail = (props) => {
   return (
     <Card>
+      <CardSection>
+        <View>
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>Alfie Bullmore</Text>
+          <Text>@makersAcademyGroup</Text>
+        </View>
+      </CardSection>
       <CardSection>
         <Text>{props.post.body}</Text>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
 }
 
 export default CardDetail;
