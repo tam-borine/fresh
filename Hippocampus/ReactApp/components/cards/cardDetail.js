5f8c4eee413636de8b3cd5aae73c9c6c0a056baa
Alfie <alfie_ab@hotmail.co.uk>
2016-12-30 16:22:45 +0000

    card section added

--- Hippocampus/ReactApp/components/cards/cardDetail.js
+++ Hippocampus/ReactApp/components/cards/cardDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './card'
+import CardSection from './cardSection'
 
 const CardDetail = (props) => {
   return (
     <Card>
-      <Text>{props.post.body}</Text>
+      <CardSection>
+        <Text>{props.post.body}</Text>
+      </CardSection>
     </Card>
   )
 }
