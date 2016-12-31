c2273fe7286a39abce46d2892aeb8e540721a99d
Alfie <alfie_ab@hotmail.co.uk>
2016-12-31 12:09:14 +0000

    mock image added

--- Hippocampus/ReactApp/components/cards/cardDetail.js
+++ Hippocampus/ReactApp/components/cards/cardDetail.js
@@ -4,14 +4,22 @@ import Card from './card'
 import CardSection from './cardSection'
 
 // Below is hard coded just for styling purposes, will need to make it flexible later
-// First view box is empty now but will be filled with image
+// URI is empty to just show where image will go
 const CardDetail = (props) => {
+
+// Destructuring styles as it's used twice, can do this later with props
+
+  const { thumbnailStyle, headerContentStyle } = styles;
+
   return (
     <Card>
       <CardSection>
         <View>
+          <Image style={thumbnailStyle}
+            source={{ uri: "" }}
+          />
         </View>
-        <View style={styles.headerContentStyle}>
+        <View style={headerContentStyle}>
           <Text>Alfie Bullmore</Text>
           <Text>@makersAcademyGroup</Text>
         </View>
@@ -27,6 +35,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 }
 
